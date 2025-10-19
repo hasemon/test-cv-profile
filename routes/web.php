a282d95fe7091ee650aa542a1eae8eed2c91a8cc
Raihanul Haque <raihancse101@gmail.com>
2025-10-20 00:11:09 +0600

    Education and comment controller added

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EducationController;
+use App\Http\Controllers\CommentController;
 
 
 // Profile Routes
-Route::get('/', [ProfileController::class,'index'])->name('profile.index');
-Route::get('/create', [ProfileController::class,'create'])->name('profile.create');
-Route::post('/store', [ProfileController::class,'store'])->name('profile.store');
-Route::get('/edit/{profile}', [ProfileController::class,'edit'])->name('profile.edit');
-Route::put('/update/{profile}', [ProfileController::class,'update'])->name('profile.update');
-Route::delete('/delete/{profile}', [ProfileController::class,'destroy'])->name('profile.destroy');
+Route::get('/', [ProfileController::class, 'index'])->name('profile.index');
+Route::get('/create', [ProfileController::class, 'create'])->name('profile.create');
+Route::post('/store', [ProfileController::class, 'store'])->name('profile.store');
+Route::get('/edit/{profile}', [ProfileController::class, 'edit'])->name('profile.edit');
+Route::put('/update/{profile}', [ProfileController::class, 'update'])->name('profile.update');
+Route::delete('/delete/{profile}', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
 
 // Education
-Route::post('/education/store', [ProfileController::class, 'storeEducation'])->name('education.store');
-Route::delete('/education/delete/{id}', [ProfileController::class, 'deleteEducation'])->name('education.delete');
+Route::post('/education/store', [EducationController::class, 'store'])->name('education.store');
+Route::delete('/education/delete/{id}', [EducationController::class, 'destroy'])->name('education.delete');
 
-// Comment
-Route::post('/comment/store', [ProfileController::class, 'storeComment'])->name('comment.store');
-Route::delete('/comment/delete/{id}', [ProfileController::class, 'deleteComment'])->name('comment.delete');
+
+// Comment 
+Route::post('/comment/store', [CommentController::class, 'store'])->name('comment.store');
+Route::delete('/comment/delete/{id}', [CommentController::class, 'destroy'])->name('comment.delete');
